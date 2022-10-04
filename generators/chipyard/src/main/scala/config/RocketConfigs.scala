aa723f58e57bd1d4b224b81a7c8502e04b9439a9
shahzaibk23 <shahzaibceo@gmail.com>
2022-10-05 02:07:38 +0500

    comment out not neede classes in config

--- generators/chipyard/src/main/scala/config/RocketConfigs.scala
+++ generators/chipyard/src/main/scala/config/RocketConfigs.scala
@@ -248,20 +248,21 @@ class MulticlockAXIOverSerialConfig extends Config(
 // DOC include end: MulticlockAXIOverSerialConfig
 
 class JustReadTLRocketConfig extends Config(
-  new chipyard.iobinders.WithUARTAdapter ++
-  new chipyard.iobinders.WithTieOffInterrupts ++
-  new chipyard.iobinders.WithBlackBoxSimMem ++
-  new chipyard.iobinders.WithTiedOffDebug ++
-  new chipyard.iobinders.WithSimSerial ++
-  new testchipip.WithTSI ++
-  new chipyard.config.WithUART ++
-  new chipyard.config.WithBootROM ++
-  new chipyard.config.WithL2TLBs(1024) ++
+  // new chipyard.iobinders.WithUARTAdapter ++
+  // new chipyard.iobinders.WithTieOffInterrupts ++
+  // new chipyard.iobinders.WithBlackBoxSimMem ++
+  // new chipyard.iobinders.WithTiedOffDebug ++
+  // new chipyard.iobinders.WithSimSerial ++
+  // new testchipip.WithTSI ++
+  // new chipyard.config.WithUART ++
+  // new chipyard.config.WithBootROM ++
+  // new chipyard.config.WithL2TLBs(1024) ++
   new chipyard.example.WithJustRead ++ // Use our MMIO peripheral, connect TL
-  new freechips.rocketchip.subsystem.WithNoMMIOPort ++
-  new freechips.rocketchip.subsystem.WithNoSlavePort ++
-  new freechips.rocketchip.subsystem.WithInclusiveCache ++
-  new freechips.rocketchip.subsystem.WithNExtTopInterrupts(0) ++
+  // new freechips.rocketchip.subsystem.WithNoMMIOPort ++
+  // new freechips.rocketchip.subsystem.WithNoSlavePort ++
+  // new freechips.rocketchip.subsystem.WithInclusiveCache ++
+  // new freechips.rocketchip.subsystem.WithNExtTopInterrupts(0) ++
   new freechips.rocketchip.subsystem.WithNBigCores(1) ++
-  new freechips.rocketchip.subsystem.WithCoherentBusTopology ++
-  new freechips.rocketchip.system.BaseConfig)
+  // new freechips.rocketchip.subsystem.WithCoherentBusTopology ++
+  new freechips.rocketchip.system.BaseConfig
+  )
