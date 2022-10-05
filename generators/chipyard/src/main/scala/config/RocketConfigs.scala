675ad729534188085ce82a1a75cb38b5be2985cd
shahzaibk23 <shahzaibceo@gmail.com>
2022-10-06 02:01:35 +0500

    clean JustReadTLRocketConfig

--- generators/chipyard/src/main/scala/config/RocketConfigs.scala
+++ generators/chipyard/src/main/scala/config/RocketConfigs.scala
@@ -248,22 +248,7 @@ class MulticlockAXIOverSerialConfig extends Config(
 // DOC include end: MulticlockAXIOverSerialConfig
 
 class JustReadTLRocketConfig extends Config(
-  // new chipyard.iobinders.WithUARTAdapter ++
-  // new chipyard.iobinders.WithTieOffInterrupts ++
-  // new chipyard.iobinders.WithBlackBoxSimMem ++
-  // new chipyard.iobinders.WithTiedOffDebug ++
-  // new chipyard.iobinders.WithSimSerial ++
-  // new testchipip.WithTSI ++
-  // new chipyard.config.WithUART ++
-  // new chipyard.config.WithBootROM ++
-  // new chipyard.config.WithL2TLBs(1024) ++
   new chipyard.example.WithJustRead(address = 0x2000, width = 32) ++ // Use our MMIO peripheral, connect TL
-  // new freechips.rocketchip.subsystem.WithNoMMIOPort ++
-  // new freechips.rocketchip.subsystem.WithNoSlavePort ++
-  // new freechips.rocketchip.subsystem.WithInclusiveCache ++
-  // new freechips.rocketchip.subsystem.WithNExtTopInterrupts(0) ++
   new freechips.rocketchip.subsystem.WithNBigCores(1) ++
-  // new freechips.rocketchip.subsystem.WithCoherentBusTopology ++
-  // new freechips.rocketchip.system.BaseConfig
   new chipyard.config.AbstractConfig
   )
